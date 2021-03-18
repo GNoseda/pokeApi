4f67de58461009ade5f9b36225f8d2ba0876477a
GNoseda <g.noseda.soto@gmail.com>
2021-03-17 22:02:24 -0300

    Modal Damage relations ready, Modal Abilitys to go

--- pokeapi.js
+++ pokeapi.js
@@ -12,8 +12,7 @@ $(document).ready(function(){
                 return response.json();
             })
             .then(function(response){ 
-                next = response.next
-                console.log(response);           
+                next = response.next        
                 response.results.forEach(function(info) {
                     
                     let card = `<div class="card" style="width: 18rem;">
@@ -61,16 +60,11 @@ $(document).ready(function(){
                              
                         </div>
 
-                        <div id="divsubmodal__${response.name}">
-                             
-                        </div>
-
-
-
-                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#divsubmodal__${response.name}">
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#submodal__${response.name}" data-bs-dismiss="modal">
                             Ver Relaciones de daño
                         </button>
                           
+                        
 
                         <div class="modal-footer">
                           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -80,15 +74,42 @@ $(document).ready(function(){
                     </div>
                   </div>`
 
-                  modal2 = `<div class="modal fade" id="divsubmodal__${response.name}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  
+                  $('#info__div').append(modal)
+                  $("#image__"+(response.name)).attr("src",response.sprites.front_default)
+                  
+                  $("#divmodal__"+(response.name)).append(readAbility(response.abilities))
+
+                  $("#divmodal__"+(response.name)).append("<br>")
+                  $("#divmodal__"+(response.name)).append(readType(response.types))
+                  $("#divmodal__"+(response.name)).append("<br>")
+                  $("#divmodal__"+(response.name)).append(readMove(response.moves))
+                  $("#divmodal__"+(response.name)).append("<br>")
+                  $("#divmodal__"+(response.name)).append(readGen(response.game_indices))
+                  
+
+                  modal2 = `<div class="modal overflow fade" id="submodal__${response.name}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                     <div class="modal-dialog">
                       <div class="modal-content">
                         <div class="modal-header">
                           <h5 class="modal-title" id="exampleModalLabel">${response.name}</h5>
                           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
-                        <div id="divmodal__${response.name}">
-                             
+                        <div id="divsubmodal__${response.name}">
+                            <h4>Double Damage To</h4>
+                            <div id="divdouble_damage_to__${response.name}"></div>
+                            <h4>Double Damage from</h4>
+                            <div id="divdouble_damage_from__${response.name}"></div>
+                            
+                            <h4>Half Damage To</h4>
+                            <div id="divhalf_damage_to__${response.name}"></div>
+                            <h4>Half Damage from</h4>
+                            <div id="divhalf_damage_from__${response.name}"></div>
+                            
+                            <h4>No Damage To</h4>
+                            <div id="divno_damage_to__${response.name}"></div>
+                            <h4>No Damage from</h4>
+                            <div id="divno_damage_from__${response.name}"></div>
                         </div>
 
                         <div class="modal-footer">
@@ -98,25 +119,87 @@ $(document).ready(function(){
                       </div>
                     </div>
                   </div>`
-                
-                $('#info__div').append(modal)
-                $("#image__"+(response.name)).attr("src",response.sprites.front_default)
- 
-                $("#divmodal__"+(response.name)).append(readAbility(response.abilities))
-                $("#divmodal__"+(response.name)).append("<br>")
-                $("#divmodal__"+(response.name)).append(readType(response.types))
-                $("#divmodal__"+(response.name)).append("<br>")
-                $("#divmodal__"+(response.name)).append(readMove(response.moves))
-                $("#divmodal__"+(response.name)).append("<br>")
-                $("#divmodal__"+(response.name)).append(readGen(response.game_indices))
 
-                $("divsubmodal__"+(response.name)).append(modal2)
+                $('#info__div').append(modal2)
+                getTypeURL(response.types, response.name)
+
+                modal3 = `<div class="modal overflow fade" id="abilitymodal__${response.name}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="exampleModalLabel">${response.name}</h5>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div id="divsubmodal__${response.name}">
+                        <h4>Ability Modal</h4>
+                        <div id="divdouble_damage_to__${response.name}"></div>
+                        <h4>Double Damage from</h4>
+                        <div id="divdouble_damage_from__${response.name}"></div>
+                        
+                        <h4>Half Damage To</h4>
+                        <div id="divhalf_damage_to__${response.name}"></div>
+                        <h4>Half Damage from</h4>
+                        <div id="divhalf_damage_from__${response.name}"></div>
+                        
+                        <h4>No Damage To</h4>
+                        <div id="divno_damage_to__${response.name}"></div>
+                        <h4>No Damage from</h4>
+                        <div id="divno_damage_from__${response.name}"></div>
+                    </div>
+
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                      <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                  </div>
+                </div>
+              </div>`
+              
+            $('#info__div').append(modal3)
             })
     }
     
+    function getTypeURL(arr,pokeName){
+        let text = ""
+        arr.forEach(function(info){
+            text = info.type.url
+            readDamage(text,pokeName)
+        })
+
+        
+    }
+
+    function readDamage(url, pokeName){
+        fetch(url)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(response){
+            //console.log(response.damage_relations)
+
+            for (var key in response.damage_relations){
+                let x = response.damage_relations[key]
+                //console.log(key)   
+                x.forEach(function(info){
+                    //console.log(info.name)
+                    $("#div"+(key)+"__"+(pokeName)).append(info.name + " ")
+
+                })
+                // if (key == "name") doSomething();
+            }
+
+            //response.damage_relations.forEach(function(info){
+            //    console.log(info)
+                //text = info.type.url
+                //readDamage(text,div)
+            //})
+            //$(div).append(text)
+        })
+    }
+
+
     function readGen(arr){
         let text = ""
-        console.log(arr)
         arr.forEach(function(info){
             text = text + " " + info.version.name
         })
@@ -126,8 +209,7 @@ $(document).ready(function(){
     function readAbility(arr){
         let text = ""
         arr.forEach(function(info){
-            console.log()
-            text = text + " " + info.ability.name
+            text = text + " " + "<li>"+(info.ability.name) + "<button type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#abilitymodal__${response.name}' data-bs-dismiss='modal'> Ver otros pokemon con la misma habilidad </button></li>" 
         })
         return text
     }
@@ -135,7 +217,7 @@ $(document).ready(function(){
     function readType(arr){
         let text = ""
         arr.forEach(function(info){
-            console.log()
+
             text = text + " " + info.type.name
         })
         return text
@@ -145,7 +227,7 @@ $(document).ready(function(){
         let text = ""
         let count = 1
         arr.forEach(function(info){
-            console.log()
+
             if(count <= 5 ) {text = text + " " + info.move.name}
             count += 1
         })
